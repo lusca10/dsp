8a458bf1a7e51b801efb1116f1472d6bb78f0f88
lusca10 <41131523+lusca10@users.noreply.github.com>
2019-06-07 05:38:45 -0300

    Add files via upload

---
+++ imoveis.java
@@ -0,0 +1,158 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+public class imoveis {
+
+    public static void main(String[] args) {
+     
+        Scanner l = new Scanner(System.in);
+        
+        System.out.println("qual imovel");
+        
+        String nome = l.nextLine();
+        
+        System.out.println("endereco");
+        
+        String endereco = l.nextLine();
+        
+        System.out.println("tamanho");
+        
+        String tamanho = l.nextLine();
+        
+        System.out.println("ano");
+        
+        int ano = l.nextInt();
+
+        System.out.println("em condiminio");
+
+        String kacondominio = l.nextLine();
+
+        //-----------------------------------------
+
+        String casa = l.nextLine();
+        String ap = ap.nextLine();
+        String imoveco = imoveco.next();        
+        //-----------------------------------------
+
+        Dados imoveis;
+    
+        imoveis = new Dados();
+        
+        imoveis.setNome(nome);
+        imoveis.setendereco(endereco);
+        imoveis.setano(ano);
+        imoveis.setkacondominio(kacondominio);
+        imoveis.setcasa(casa);
+        imoveis.setap(ap);
+        imoveis.setimoveco(imoveco);
+
+
+
+//public void setNomeProduto(Stringnomeproduto) {
+//this.nomeproduto = nomeproduto;
+//}
+
+public class imo{
+	
+
+            private String endereco;
+            private String tamanho;
+            private String ano;
+            private String kacondominio;
+
+            private String casa;
+            private String ap;
+            private String imoveco;
+             
+            
+            public String getImoveco(){
+                return imoveco;
+            }
+
+            public String getAp(){
+                return ap;
+            }
+
+            public String getCasa(){
+                return casa;
+            }
+
+             public String getEndereco(){
+   
+                return endereco;
+            }
+
+            public String getTamanho(){
+                return tamanho;
+            }
+
+            public String getAno(){
+                return ano;
+            }
+            return String getKacondominio(){
+                return kacondominio;
+            }
+            
+            public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    
+    public void setTamanho(String tamanho) {
+        this.tamanho = tamanho;
+    }
+    
+    public void setAno(String ano) {
+        this.ano = ano;
+    }
+    
+    public void setKacondominio(String kacondominio) {
+        this.kacondominio = kacondminio;
+    }
+    
+    public void setImoveco(String imoveco) {
+        this.imoveco = imoveco;
+    }
+
+	public void setaCasa(String casa) {
+        this.casa = casa;
+    }
+
+
+	public void setAp(String ap) {
+        this.ap = ap;
+    }
+        
+}
+
+}
+   
+
+   if (nome == casa) {
+
+       System.out.println(tamanho * 7 + "imposto é roubo");
+   }
+        
+
+   if (nome == imoveco) {
+
+       System.out.println(tamanho * 10 + "imposto é roubo");
+   }
+
+
+   if (nome == ap) {
+
+       System.out.println(tamanho *  8 + "imposto é roubo");
+   }
+    
+
+   if (nome == kacondominio ) {
+
+       System.out.println(tamanho * 9 + "imposto é roubo");
+   }
+    
+        
+        
+        
+       
+     }
+}
+    
